369ef7614977c9d451ea4bdba9f5da69b3e0c559
Ryan Tanay <ryantanay@newevolution.org>
2015-07-22 11:44:22 -0500

    Pulled the main outline text out of the JS since that's what I'd set to the README anyway.

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 /*
-  .....  YOUR MISSION  .....
-  1. Author functions that define the Make
-      and Model of a Car and the Car itself.
-  2. Create three instances of your Car that
-      each have a different color.
-  3. Each Car you create should inherit the
-      make and model name.
-*/
-
-
-/*
   Create a function, Make, that has a property
    of makeName and give it the value of your
    vehicle's make.
